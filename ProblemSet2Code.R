215f0831a5250f08ccb2fe5b6b5ce05adcf4d978
adrielbarrettjo <galadriel@barrett-johnson.net>
2015-11-02 23:19:22 -0500

    update psets

--- ProblemSet2Code.R
+++ ProblemSet2Code.R
@@ -36,20 +36,40 @@ whig <- c(1841, 1849, 1850)
 federalist <- c(1797)
 independent <- c(1789)
 
+# Create vector for all
+all <- c(1861, 1865, 1869, 1877, 1881, 1889, 1897, 1901, 1909, 1921, 1923, 1929, 1953, 1969, 1974, 1981, 1989, 2001,1801, 1809, 1817, 1825, 1829, 1837, 1845, 1853, 1857, 1865, 1885, 1893, 1913, 1933, 1945, 1961, 1963, 1977, 1993, 2009, 1841, 1849, 1850, 1797, 1789)
+
 # subset the datasheet into a sheet for each party in history
 inaugural.republican <- subset(x=inaugural, link %in% republican)
 inaugural.democrat <- subset(x=inaugural, link %in% democrat)
 inaugural.whig <- subset(x=inaugural, link %in% whig)
 inaugural.federalist <- subset(x=inaugural, link %in% federalist)
 inaugural.independent <- subset(x=inaugural, link %in% independent)
+inaugural.all <- subset(x=inaugural, link %in% all)
+
+# Get text for all so can make corpus
+for(i in seq(nrow(inaugural.all))) {
+  text.all <- read_html(inaugural.all$url[i]) %>% # load the page
+    html_nodes(".displaytext") %>% # isloate the text
+    html_text() # get the text
+  
+party <- ifelse(test = inaugural.all$link[i] %in% all,
+                  yes = "early", no = "late")
+  
+  # Create the file name
+  filename <- paste0(party, "-", inaugural.all$links[i], ".txt")
+  # and send the output to an appropriately named file
+  sink(file = filename) %>% # open file to write 
+    cat(text.all)  # write the file
+  sink() # close the file
+}
 
 ###### get text for republican
 for(i in seq(nrow(inaugural.republican))) {
   text.republican <- read_html(inaugural.republican$url[i]) %>% # load the page
     html_nodes(".displaytext") %>% # isloate the text
     html_text() # get the text
-  
-  # Find the time period of this link ...this doesn't make sense but it works?
+
   party <- ifelse(test = inaugural.republican$link[i] %in% republican,
                   yes = "early", no = "late")
   
@@ -67,7 +87,6 @@ for(i in seq(nrow(inaugural.whig))) {
     html_nodes(".displaytext") %>% # isloate the text
     html_text() # get the text
   
-  # Find the time period of this link ...this doesn't make sense but it works?
   party <- ifelse(test = inaugural.whig$link[i] %in% whig,
                   yes = "early", no = "late")
   
@@ -85,7 +104,6 @@ for(i in seq(nrow(inaugural.independent))) {
     html_nodes(".displaytext") %>% # isloate the text
     html_text() # get the text
   
-  # Find the time period of this link ...this doesn't make sense but it works?
   party <- ifelse(test = inaugural.independent$link[i] %in% independent,
                   yes = "early", no = "late")
   
@@ -103,7 +121,6 @@ for(i in seq(nrow(inaugural.federalist))) {
     html_nodes(".displaytext") %>% # isloate the text
     html_text() # get the text
   
-  # Find the time period of this link ...this doesn't make sense but it works?
   party <- ifelse(test = inaugural.federalist$link[i] %in% federalist,
                   yes = "early", no = "late")
   
@@ -133,22 +150,28 @@ for(i in seq(nrow(inaugural.democrat))) {
   sink() # close the file
 }
 
-
+########### clean up the text for all of the inaugural addresses first
+allcor <- Corpus(VectorSource(text.all))
 ########### clean up the text for only the democrats AND republicans ################
 # Create a corpus
 dem <- Corpus(VectorSource(text.democrat))
 rep <- Corpus(VectorSource(text.republican))
 # Remove capitalization
+allcor <- tm_map(allcor,content_transformer(tolower)) 
 dem <- tm_map(dem,content_transformer(tolower)) 
 rep <- tm_map(rep,content_transformer(tolower)) 
 
 # create spaces so can accurately remove all punctuation
 toSpace <- content_transformer(function(x, pattern) 
-  {return (gsub(pattern, "  ", x))})
+{return (gsub(pattern, "  ", x))})
+allcor <- tm_map(allcor, toSpace, "-")
 dem <- tm_map(dem, toSpace, "-")
 rep <- tm_map(rep, toSpace, "-")
-  
+
 # Remove punctuation
+allcor <- tm_map(allcor, removePunctuation)
+allcor[[1]][1]
+
 dem <- tm_map(dem, removePunctuation) 
 dem[[1]][1]
 
@@ -156,6 +179,8 @@ rep <- tm_map(rep, removePunctuation)
 rep[[1]][1]
 
 # Remove stop words because we don't need them here
+allcor <- tm_map(allcor, removeWords, stopwords("english"))
+
 dem <- tm_map(dem, removeWords, stopwords("english")) 
 dem[[1]][1]
 
@@ -163,6 +188,8 @@ rep <- tm_map(rep, removeWords, stopwords("english"))
 rep[[1]][1]
 
 # Strip whitespace
+allcor <- tm_map(allcor, stripWhitespace)
+
 dem<- tm_map(dem, stripWhitespace)
 dem[[1]][1]
 
@@ -170,14 +197,12 @@ rep <- tm_map(rep, stripWhitespace)
 rep[[1]][1]
 
 library(wordcloud)
-wordcloud(rep, min.freq=1, scale=c(3,.25))
-
-library(wordcloud)
-wordcloud(dem, min.freq=1, scale=c(3,.25))
 
 # Stemming
 install.packages("SnowballC")
 library(SnowballC)
+
+allStem <- tm_map(allcor, stemDocument) ######################################
 
 demStem <- tm_map(dem, stemDocument)
 demStem[[1]][1]
@@ -191,7 +216,6 @@ wordStem(c("smaller", "small"))
 wordStem(c("bigger", "big"))
 wordStem(c("children", "child"))
 wordStem(c("citizenship", "citizen"))
-wordStem(c("failure" "fail"))
 wordStem(c("worker", "work"))
 wordStem(c("woman", "women"))
 wordStem(c("willing", "will"))
@@ -204,6 +228,10 @@ wordStem(c("surest", "sure"))
 
 
 # Create Term Document Matrix
+allTDM <- TermDocumentMatrix(allStem)
+inspect <- (allTDM)
+str (allTDM)
+
 repTDM <- TermDocumentMatrix(repStem)
 inspect(repTDM)
 str(repTDM)
@@ -224,6 +252,29 @@ repFreq <- findFreqTerms(repTDM, lowfreq=5, highfreq=Inf)
 repFreq
 ## lowfreq=5, get 27 words 
 
-##
+## wordcloud for each individually 
+wordcloud(allcor, max.words=100, scale=c(3,.25))
 wordcloud(dem, max.words=100, scale=c(3,.25))
 wordcloud(rep, max.words=100, scale=c(3,.25))
+
+# Remove sparse/uncommon terms
+allcorcommon <- removeSparseTerms(allTDM, 0.85) # This makes a matrix that is only 85% empty space, maximum.
+dim(allTDM)
+dim(allcorcommon)
+
+## vizualization of freq terms after removing sparse terms
+wordcloud(allcorcommon, max.words=25, scale=c(3,.25))
+
+# wordcloud to compare dems and reps
+allcorC <- as.matrix(allcorcommon)
+allcorC[,] 
+comparison.cloud(allcorC[,], max.words=100, 
+                 scale=c(3,.25), random.order=FALSE, 
+                 colors=c("blue3", "red3"), title.size=1)
+
+# initial observations comparing the reps and dems:
+# Democrats use a lot of words like can, courage, new, common, seek, will, etc.
+# In general, these are future slanted words as well as focused on the individual 
+# person.
+# Republicans use words like character, defend, responsibility, commitment, etc.
+# These are words I would expect from someone trying to appeal to conservatives.
